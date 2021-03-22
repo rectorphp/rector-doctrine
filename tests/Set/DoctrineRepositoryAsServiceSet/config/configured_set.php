0f6b47a74d2b587729a8e8f60a2607a84eed1039
TomasVotruba <tomas.vot@gmail.com>
2021-03-22 01:41:34 +0100

    add stubs

--- tests/Set/DoctrineRepositoryAsServiceSet/config/configured_set.php
+++ tests/Set/DoctrineRepositoryAsServiceSet/config/configured_set.php
@@ -6,5 +6,7 @@ use Rector\Doctrine\Set\DoctrineSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../../../../config/config.php');
+
     $containerConfigurator->import(DoctrineSetList::DOCTRINE_REPOSITORY_AS_SERVICE);
 };
