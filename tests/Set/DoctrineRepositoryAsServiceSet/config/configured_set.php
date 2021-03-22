8269b19573039a6025a41619451907944e9b4bbe
TomasVotruba <tomas.vot@gmail.com>
2021-03-22 01:23:18 +0100

    load stubs

--- tests/Set/DoctrineRepositoryAsServiceSet/config/configured_set.php
+++ tests/Set/DoctrineRepositoryAsServiceSet/config/configured_set.php
@@ -2,11 +2,9 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Set\ValueObject\SetList;
+use Rector\Doctrine\Set\DoctrineSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SETS, [SetList::DOCTRINE_REPOSITORY_AS_SERVICE]);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_REPOSITORY_AS_SERVICE);
 };
