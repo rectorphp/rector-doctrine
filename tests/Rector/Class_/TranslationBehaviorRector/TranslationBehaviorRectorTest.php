015a4b2baf9fc2294c402962f28c08ed1c71e5a6
TomasVotruba <tomas.vot@gmail.com>
2021-04-13 10:17:10 +0200

    upgrade deprecated test extra file with assertFileWasAdded()

--- tests/Rector/Class_/TranslationBehaviorRector/TranslationBehaviorRectorTest.php
+++ tests/Rector/Class_/TranslationBehaviorRector/TranslationBehaviorRectorTest.php
@@ -4,17 +4,37 @@ declare(strict_types=1);
 
 namespace Rector\Doctrine\Tests\Rector\Class_\TranslationBehaviorRector;
 
+use Iterator;
+use Rector\FileSystemRector\ValueObject\AddedFileWithContent;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 use Symplify\SmartFileSystem\SmartFileInfo;
+use Symplify\SmartFileSystem\SmartFileSystem;
 
 final class TranslationBehaviorRectorTest extends AbstractRectorTestCase
 {
-    public function test(): void
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fixtureFileInfo, AddedFileWithContent $expectedAddedFileWithContent): void
     {
-        $fitureFileInfo = new SmartFileInfo(__DIR__ . '/Fixture/fixture.php.inc');
-        $this->doTestFileInfo($fitureFileInfo);
+        $this->doTestFileInfo($fixtureFileInfo);
+        $this->assertFileWasAdded($expectedAddedFileWithContent);
+    }
 
-        $this->doTestExtraFile('SomeClassTranslation.php', __DIR__ . '/Source/SomeClassTranslation.php');
+    /**
+     * @return Iterator<array<SmartFileInfo|AddedFileWithContent>>
+     */
+    public function provideData(): Iterator
+    {
+        $smartFileSystem = new SmartFileSystem();
+
+        yield [
+            new SmartFileInfo(__DIR__ . '/Fixture/fixture.php.inc'),
+            new AddedFileWithContent(
+                'SomeClassTranslation.php',
+                $smartFileSystem->readFile(__DIR__ . '/Source/SomeClassTranslation.php')
+            ),
+        ];
     }
 
     public function provideConfigFilePath(): string
