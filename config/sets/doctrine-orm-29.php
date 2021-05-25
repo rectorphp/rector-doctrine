c3699976e4a4c41aae97bb87b0325f46266d771f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-05-25 08:17:53 +0700

    apply auto import for short names

--- config/sets/doctrine-orm-29.php
+++ config/sets/doctrine-orm-29.php
@@ -6,8 +6,12 @@ use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\SymfonyPhpConfig\ValueObjectInliner;
+use Rector\Core\Configuration\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+
     $services = $containerConfigurator->services();
 
     $services->set(AnnotationToAttributeRector::class)
