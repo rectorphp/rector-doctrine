4986acb52c6a086489e7dbf5055a47a89860bea0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-05-25 07:52:08 +0700

    [Set] Add doctrine orm 2.9 set

---
+++ config/sets/doctrine-orm-29.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+use Rector\Php80\ValueObject\AnnotationToAttribute;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(AnnotationToAttributeRector::class)
+             ->call('configure',  [[
+                AnnotationToAttributeRector::ANNOTATION_TO_ATTRIBUTE => ValueObjectInliner::inline(
+                    [
+                        new AnnotationToAttribute('Doctrine\ORM\Mapping\Table', 'Doctrine\ORM\Mapping\Table'),
+                        new AnnotationToAttribute('Doctrine\ORM\Mapping\Entity', 'Doctrine\ORM\Mapping\Entity'),
+                        new AnnotationToAttribute('Doctrine\ORM\Mapping\Id', 'Doctrine\ORM\Mapping\Id'),
+                        new AnnotationToAttribute('Doctrine\ORM\Mapping\GeneratedValue', 'Doctrine\ORM\Mapping\GeneratedValue'),
+                        new AnnotationToAttribute('Doctrine\ORM\Mapping\Column', 'Doctrine\ORM\Mapping\Column'),
+                    ]
+                )
+             ]]);
+};
