6a9924a8cd3c5dd00cadd9b8f72e95453e8ab3b4
Tomas Votruba <tomas.vot@gmail.com>
2021-06-15 11:39:03 +0200

    add public property supports

---
+++ src/ValueObject/PropertyNameAndPhpDocInfo.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Doctrine\ValueObject;
+
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
+
+final class PropertyNameAndPhpDocInfo
+{
+    public function __construct(
+        private string $propertyName,
+        private PhpDocInfo $phpDocInfos
+    ) {
+    }
+
+    public function getPropertyName(): string
+    {
+        return $this->propertyName;
+    }
+
+    public function getPhpDocInfo(): PhpDocInfo
+    {
+        return $this->phpDocInfos;
+    }
+}
