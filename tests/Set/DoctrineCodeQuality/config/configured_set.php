58edad92cf91e5895e7086baf7e150e0f57e1169
wkania <wojciech.kania+github@gmail.com>
2021-09-29 19:07:43 +0200

    [Set] Replace addOrderBy order with a constant (#29)

---
+++ tests/Set/DoctrineCodeQuality/config/configured_set.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Doctrine\Set\DoctrineSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../../../../config/config.php');
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+};
