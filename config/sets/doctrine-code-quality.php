8123f751a17ef0a4916bb94a87d6aaefa2eead18
Tomas Votruba <tomas.vot@gmail.com>
2021-10-14 17:21:50 +0200

    [CodeQuality] Add Column types to constants (#36)

--- config/sets/doctrine-code-quality.php
+++ config/sets/doctrine-code-quality.php
@@ -14,7 +14,9 @@ use Rector\Doctrine\Rector\Property\MakeEntityDateTimePropertyDateTimeInterfaceR
 use Rector\Doctrine\Rector\Property\RemoveRedundantDefaultPropertyAnnotationValuesRector;
 use Rector\Privatization\Rector\MethodCall\ReplaceStringWithClassConstantRector;
 use Rector\Privatization\ValueObject\ReplaceStringWithClassConstant;
+use Rector\Transform\Rector\Attribute\AttributeKeyToClassConstFetchRector;
 use Rector\Transform\Rector\MethodCall\ServiceGetterToConstructorInjectionRector;
+use Rector\Transform\ValueObject\AttributeKeyToClassConstFetch;
 use Rector\Transform\ValueObject\ServiceGetterToConstructorInjection;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\SymfonyPhpConfig\ValueObjectInliner;
@@ -31,6 +33,40 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(ImproveDoctrineCollectionDocTypeInEntityRector::class);
     $services->set(RemoveRedundantDefaultPropertyAnnotationValuesRector::class);
     $services->set(RemoveRedundantDefaultClassAnnotationValuesRector::class);
+
+    $services = $containerConfigurator->services();
+    $services->set(AttributeKeyToClassConstFetchRector::class)
+        ->call('configure', [[
+            AttributeKeyToClassConstFetchRector::ATTRIBUTE_KEYS_TO_CLASS_CONST_FETCHES => ValueObjectInliner::inline([
+                new AttributeKeyToClassConstFetch('Doctrine\ORM\Mapping\Column', 'type', 'Doctrine\DBAL\Types\Types', [
+                    'array' => 'ARRAY',
+                    'ascii_string' => 'ASCII_STRING',
+                    'bigint' => 'BIGINT',
+                    'binary' => 'BINARY',
+                    'blob' => 'BLOB',
+                    'boolean' => 'BOOLEAN',
+                    'date' => 'DATE_MUTABLE',
+                    'date_immutable' => 'DATE_IMMUTABLE',
+                    'dateinterval' => 'DATEINTERVAL',
+                    'datetime' => 'DATETIME_MUTABLE',
+                    'datetime_immutable' => 'DATETIME_IMMUTABLE',
+                    'datetimetz' => 'DATETIMETZ_MUTABLE',
+                    'datetimetz_immutable' => 'DATETIMETZ_IMMUTABLE',
+                    'decimal' => 'DECIMAL',
+                    'float' => 'FLOAT',
+                    'guid' => 'GUID',
+                    'integer' => 'INTEGER',
+                    'json' => 'JSON',
+                    'object' => 'OBJECT',
+                    'simple_array' => 'SIMPLE_ARRAY',
+                    'smallint' => 'SMALLINT',
+                    'string' => 'STRING',
+                    'text' => 'TEXT',
+                    'time' => 'TIME_MUTABLE',
+                    'time_immutable' => 'TIME_IMMUTABLE',
+                ]),
+            ]),
+        ]]);
 
     $services->set(ReplaceStringWithClassConstantRector::class)
         ->call('configure', [[
