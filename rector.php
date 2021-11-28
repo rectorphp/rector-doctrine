9d30329421ca249e6e745010f25a21609289ed0a
Tomas Votruba <tomas.vot@gmail.com>
2021-11-29 01:58:47 +0300

    Add Rector sets (#51)

--- rector.php
+++ rector.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -22,7 +24,19 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         '*/Fixture/*',
     ]);
 
-    $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(SetList::PHP_74);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
     $containerConfigurator->import(SetList::DEAD_CODE);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
+
+    $services = $containerConfigurator->services();
+    $services->set(StringClassNameToClassConstantRector::class)
+        ->call('configure', [[
+            StringClassNameToClassConstantRector::CLASSES_TO_SKIP => [
+                'Doctrine\*',
+                'Gedmo\*',
+                'Knp\*',
+                'DateTime',
+                'DateTimeInterface',
+            ],
+        ]]);
 };
