09c5d6846ad600ddd3b7e15335324afe1023142e
Tomas Votruba <tomas.vot@gmail.com>
2021-12-19 12:09:04 +0100

    comment

--- config/sets/doctrine-annotations-to-attributes.php
+++ config/sets/doctrine-annotations-to-attributes.php
@@ -11,32 +11,46 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(AnnotationToAttributeRector::class)
         ->configure([
+            // class
             new AnnotationToAttribute('Doctrine\ORM\Mapping\Table'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\Entity'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\Column'),
+            new AnnotationToAttribute('Doctrine\ORM\Mapping\UniqueConstraint'),
+
+            // id
             new AnnotationToAttribute('Doctrine\ORM\Mapping\Id'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\GeneratedValue'),
+            new AnnotationToAttribute('Doctrine\ORM\Mapping\SequenceGenerator'),
+            new AnnotationToAttribute('Doctrine\ORM\Mapping\Index'),
+            new AnnotationToAttribute('Doctrine\ORM\Mapping\CustomIdGenerator'),
+
+            // relations
             new AnnotationToAttribute('Doctrine\ORM\Mapping\OneToOne'),
+            new AnnotationToAttribute('Doctrine\ORM\Mapping\OneToMany'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\ManyToMany'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\JoinTable'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\ManyToOne'),
+
+            // join columns
             new AnnotationToAttribute('Doctrine\ORM\Mapping\JoinColumns'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\JoinColumn'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\InverseJoinColumn'),
-            new AnnotationToAttribute('Doctrine\ORM\Mapping\OneToMany'),
-            new AnnotationToAttribute('Doctrine\ORM\Mapping\UniqueConstraint'),
-            new AnnotationToAttribute('Doctrine\ORM\Mapping\SequenceGenerator'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\OrderBy'),
-            new AnnotationToAttribute('Doctrine\ORM\Mapping\Index'),
-            new AnnotationToAttribute('Doctrine\ORM\Mapping\CustomIdGenerator'),
+
+            // embed
             new AnnotationToAttribute('Doctrine\ORM\Mapping\Embeddable'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\Embedded'),
+
+            // inheritance
             new AnnotationToAttribute('Doctrine\ORM\Mapping\MappedSuperclass'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\InheritanceType'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\DiscriminatorColumn'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\DiscriminatorMap'),
+
             new AnnotationToAttribute('Doctrine\ORM\Mapping\Version'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\ChangeTrackingPolicy'),
+
+            // events
             new AnnotationToAttribute('Doctrine\ORM\Mapping\HasLifecycleCallbacks'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\PostLoad'),
             new AnnotationToAttribute('Doctrine\ORM\Mapping\PostPersist'),
