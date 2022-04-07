2700e80a777de6bb33a57d409fc771989581a4a2
Tomas Votruba <tomas.vot@gmail.com>
2022-04-07 09:32:45 +0200

    add local config, as plugin only installs vendor packages

--- rector.php
+++ rector.php
@@ -2,16 +2,15 @@
 
 declare(strict_types=1);
 
-use Rector\Doctrine\NodeAnalyzer\AttrinationFinder;
 use Rector\Core\Configuration\Option;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/config/config.php');
+
     $parameters = $containerConfigurator->parameters();
 
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
