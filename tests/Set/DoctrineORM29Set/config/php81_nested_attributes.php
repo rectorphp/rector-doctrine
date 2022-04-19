925520bf9e24bc0d8a19481133aabec39cbb1d94
GitHub Action <action@github.com>
2022-04-19 14:34:22 +0000

    [ci-review] Rector Rectify

--- tests/Set/DoctrineORM29Set/config/php81_nested_attributes.php
+++ tests/Set/DoctrineORM29Set/config/php81_nested_attributes.php
@@ -4,15 +4,11 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
-use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\Doctrine\Set\DoctrineSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        __DIR__ . '/../../../../config/config.php',
-        DoctrineSetList::DOCTRINE_ORM_29
-    ]);
+    $rectorConfig->sets([__DIR__ . '/../../../../config/config.php', DoctrineSetList::DOCTRINE_ORM_29]);
 
     $rectorConfig->phpVersion(PhpVersionFeature::NEW_INITIALIZERS);
 };
