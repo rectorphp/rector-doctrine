4735a0ff941a07c6434676cf5f1d2176c4c81ebf
Dominik Ritter <dritter03@googlemail.com>
2022-05-05 18:59:59 +0200

    Make Doctrine Relation Stubs accept the same constructor arguments than the real classes

--- stubs/Doctrine/ORM/Mapping/ManyToMany.php
+++ stubs/Doctrine/ORM/Mapping/ManyToMany.php
@@ -4,18 +4,19 @@ declare(strict_types=1);
 
 namespace Doctrine\ORM\Mapping;
 
-if (class_exists('Doctrine\ORM\Mapping\OneToMany')) {
+if (class_exists('Doctrine\ORM\Mapping\ManyToMany')) {
     return;
 }
 
 /**
- * @see https://github.com/doctrine/orm/blob/2.12.x/lib/Doctrine/ORM/Mapping/OneToMany.php
+ * @see https://github.com/doctrine/orm/blob/2.12.x/lib/Doctrine/ORM/Mapping/ManyToMany.php
  */
-final class OneToMany
+final class ManyToMany
 {
     public function __construct(
-        ?string $mappedBy = null,
         ?string $targetEntity = null,
+        ?string $mappedBy = null,
+        ?string $inversedBy = null,
         ?array $cascade = null,
         string $fetch = 'LAZY',
         bool $orphanRemoval = false,
