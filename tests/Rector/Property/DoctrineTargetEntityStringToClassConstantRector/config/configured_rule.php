ec7839a71b9cf49e9f9148ed7e18337d1c9b97bf
Dominik Ritter <dritter03@googlemail.com>
2022-05-10 20:04:08 +0200

    Import default config in Test

--- tests/Rector/Property/DoctrineTargetEntityStringToClassConstantRector/config/configured_rule.php
+++ tests/Rector/Property/DoctrineTargetEntityStringToClassConstantRector/config/configured_rule.php
@@ -6,5 +6,6 @@ use Rector\Config\RectorConfig;
 use Rector\Doctrine\Rector\Property\DoctrineTargetEntityStringToClassConstantRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
     $rectorConfig->rule(DoctrineTargetEntityStringToClassConstantRector::class);
 };
