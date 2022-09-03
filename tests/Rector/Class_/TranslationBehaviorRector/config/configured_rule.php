44bdbabc8ec7cd0873ad28bb0c463f4c9b684b0a
Tomas Votruba <tomas.vot@gmail.com>
2022-09-03 09:43:37 +0200

    cs

--- tests/Rector/Class_/TranslationBehaviorRector/config/configured_rule.php
+++ tests/Rector/Class_/TranslationBehaviorRector/config/configured_rule.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-
 use Rector\Doctrine\Rector\Class_\TranslationBehaviorRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
-
     $rectorConfig->rule(TranslationBehaviorRector::class);
 };
