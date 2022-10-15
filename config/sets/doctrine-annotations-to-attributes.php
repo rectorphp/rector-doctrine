8fbe21e49ca599905dc7d72febfa1252207dde47
Tomas Votruba <tomas.vot@gmail.com>
2022-10-15 23:38:10 +0200

    Fix extra import on nested attributes (#142)
    * fix extra import on nested attributes
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

--- config/sets/doctrine-annotations-to-attributes.php
+++ config/sets/doctrine-annotations-to-attributes.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php80\Rector\Property\NestedAnnotationToAttributeRector;
+use Rector\Php80\ValueObject\AnnotationPropertyToAttributeClass;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
 use Rector\Php80\ValueObject\NestedAnnotationToAttribute;
 
@@ -12,18 +13,22 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(NestedAnnotationToAttributeRector::class, [
         /** @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.13/reference/attributes-reference.html#joincolumn-inversejoincolumn */
         new NestedAnnotationToAttribute('Doctrine\ORM\Mapping\JoinTable', [
-            'joinColumns' => 'Doctrine\ORM\Mapping\JoinColumn',
-            'inverseJoinColumns' => 'Doctrine\ORM\Mapping\InverseJoinColumn',
+            new AnnotationPropertyToAttributeClass('Doctrine\ORM\Mapping\JoinColumn', 'joinColumns'),
+            new AnnotationPropertyToAttributeClass(
+                'Doctrine\ORM\Mapping\InverseJoinColumn',
+                'inverseJoinColumns',
+                true
+            ),
         ]),
 
         /** @see https://www.doctrine-project.org/projects/doctrine-orm/en/latest/reference/annotations-reference.html#joincolumns */
         new NestedAnnotationToAttribute('Doctrine\ORM\Mapping\JoinColumns', [
-            'Doctrine\ORM\Mapping\JoinColumn',
+            new AnnotationPropertyToAttributeClass('Doctrine\ORM\Mapping\JoinColumn'),
         ], true),
 
         new NestedAnnotationToAttribute('Doctrine\ORM\Mapping\Table', [
-            'indexes' => 'Doctrine\ORM\Mapping\Index',
-            'uniqueConstraints' => 'Doctrine\ORM\Mapping\UniqueConstraint',
+            new AnnotationPropertyToAttributeClass('Doctrine\ORM\Mapping\Index', 'indexes', true),
+            new AnnotationPropertyToAttributeClass('Doctrine\ORM\Mapping\UniqueConstraint', 'uniqueConstraints', true),
         ]),
     ]);
 
