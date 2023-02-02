d38297b23e96e81a03d91b89904dbaf1500ffb14
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-02 17:07:00 +0700

    add missing easy-ci.php

---
+++ easy-ci.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCI\Config\EasyCIConfig;
+
+return static function (EasyCIConfig $easyCIConfig): void {
+    $easyCIConfig->paths([
+         __DIR__ . '/config',
+         __DIR__ . '/src',
+    ]);
+};
