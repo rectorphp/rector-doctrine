9d61e8210d3bf4c05951b714be632b64152dc719
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-02-02 17:09:04 +0700

    fix

--- easy-ci.php
+++ easy-ci.php
@@ -6,7 +6,6 @@ use Rector\Doctrine\Rector\Class_\ClassAnnotationToNamedArgumentConstructorRecto
 use Rector\Doctrine\Rector\MethodCall\ChangeSetParametersArrayToArrayCollectionRector;
 use Rector\Doctrine\Rector\MethodCall\EntityAliasToClassConstantReferenceRector;
 use Rector\Doctrine\Rector\Property\DoctrineTargetEntityStringToClassConstantRector;
-
 use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
