4566c67e84ebf8cb2900c2aa7015d630ae1bcb37
Tomas Votruba <tomas.vot@gmail.com>
2023-07-13 14:34:31 +0200

    use class name directly

--- config/sets/doctrine-dbal-210.php
+++ config/sets/doctrine-dbal-210.php
@@ -8,33 +8,106 @@ use Rector\Renaming\ValueObject\RenameClassAndConstFetch;
 
 return static function (RectorConfig $rectorConfig): void {
     # https://github.com/doctrine/dbal/blob/master/UPGRADE.md#deprecated-type-constants
-    $oldClass = 'Doctrine\\DBAL\\Types\\Type';
-    $newClass = 'Doctrine\\DBAL\\Types\\Types';
     $rectorConfig->ruleWithConfiguration(RenameClassConstFetchRector::class, [
-        new RenameClassAndConstFetch($oldClass, 'TARRAY', $newClass, 'ARRAY'),
-        new RenameClassAndConstFetch($oldClass, 'SIMPLE_ARRAY', $newClass, 'SIMPLE_ARRAY'),
-        new RenameClassAndConstFetch($oldClass, 'JSON_ARRAY', $newClass, 'JSON_ARRAY'),
-        new RenameClassAndConstFetch($oldClass, 'JSON', $newClass, 'JSON'),
-        new RenameClassAndConstFetch($oldClass, 'BIGINT', $newClass, 'BIGINT'),
-        new RenameClassAndConstFetch($oldClass, 'BOOLEAN', $newClass, 'BOOLEAN'),
-        new RenameClassAndConstFetch($oldClass, 'DATETIME', $newClass, 'DATETIME_MUTABLE'),
-        new RenameClassAndConstFetch($oldClass, 'DATETIME_IMMUTABLE', $newClass, 'DATETIME_IMMUTABLE'),
-        new RenameClassAndConstFetch($oldClass, 'DATETIMETZ', $newClass, 'DATETIMETZ_MUTABLE'),
-        new RenameClassAndConstFetch($oldClass, 'DATETIMETZ_IMMUTABLE', $newClass, 'DATETIMETZ_IMMUTABLE'),
-        new RenameClassAndConstFetch($oldClass, 'DATE', $newClass, 'DATE_MUTABLE'),
-        new RenameClassAndConstFetch($oldClass, 'DATE_IMMUTABLE', $newClass, 'DATE_IMMUTABLE'),
-        new RenameClassAndConstFetch($oldClass, 'TIME', $newClass, 'TIME_MUTABLE'),
-        new RenameClassAndConstFetch($oldClass, 'TIME_IMMUTABLE', $newClass, 'TIME_IMMUTABLE'),
-        new RenameClassAndConstFetch($oldClass, 'DECIMAL', $newClass, 'DECIMAL'),
-        new RenameClassAndConstFetch($oldClass, 'INTEGER', $newClass, 'INTEGER'),
-        new RenameClassAndConstFetch($oldClass, 'OBJECT', $newClass, 'OBJECT'),
-        new RenameClassAndConstFetch($oldClass, 'SMALLINT', $newClass, 'SMALLINT'),
-        new RenameClassAndConstFetch($oldClass, 'STRING', $newClass, 'STRING'),
-        new RenameClassAndConstFetch($oldClass, 'TEXT', $newClass, 'TEXT'),
-        new RenameClassAndConstFetch($oldClass, 'BINARY', $newClass, 'BINARY'),
-        new RenameClassAndConstFetch($oldClass, 'BLOB', $newClass, 'BLOB'),
-        new RenameClassAndConstFetch($oldClass, 'FLOAT', $newClass, 'FLOAT'),
-        new RenameClassAndConstFetch($oldClass, 'GUID', $newClass, 'GUID'),
-        new RenameClassAndConstFetch($oldClass, 'DATEINTERVAL', $newClass, 'DATEINTERVAL'),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'TARRAY', 'Doctrine\\DBAL\\Types\\Types', 'ARRAY'),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'SIMPLE_ARRAY',
+            'Doctrine\\DBAL\\Types\\Types',
+            'SIMPLE_ARRAY'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'JSON_ARRAY',
+            'Doctrine\\DBAL\\Types\\Types',
+            'JSON_ARRAY'
+        ),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'JSON', 'Doctrine\\DBAL\\Types\\Types', 'JSON'),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'BIGINT', 'Doctrine\\DBAL\\Types\\Types', 'BIGINT'),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'BOOLEAN',
+            'Doctrine\\DBAL\\Types\\Types',
+            'BOOLEAN'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'DATETIME',
+            'Doctrine\\DBAL\\Types\\Types',
+            'DATETIME_MUTABLE'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'DATETIME_IMMUTABLE',
+            'Doctrine\\DBAL\\Types\\Types',
+            'DATETIME_IMMUTABLE'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'DATETIMETZ',
+            'Doctrine\\DBAL\\Types\\Types',
+            'DATETIMETZ_MUTABLE'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'DATETIMETZ_IMMUTABLE',
+            'Doctrine\\DBAL\\Types\\Types',
+            'DATETIMETZ_IMMUTABLE'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'DATE',
+            'Doctrine\\DBAL\\Types\\Types',
+            'DATE_MUTABLE'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'DATE_IMMUTABLE',
+            'Doctrine\\DBAL\\Types\\Types',
+            'DATE_IMMUTABLE'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'TIME',
+            'Doctrine\\DBAL\\Types\\Types',
+            'TIME_MUTABLE'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'TIME_IMMUTABLE',
+            'Doctrine\\DBAL\\Types\\Types',
+            'TIME_IMMUTABLE'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'DECIMAL',
+            'Doctrine\\DBAL\\Types\\Types',
+            'DECIMAL'
+        ),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'INTEGER',
+            'Doctrine\\DBAL\\Types\\Types',
+            'INTEGER'
+        ),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'OBJECT', 'Doctrine\\DBAL\\Types\\Types', 'OBJECT'),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'SMALLINT',
+            'Doctrine\\DBAL\\Types\\Types',
+            'SMALLINT'
+        ),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'STRING', 'Doctrine\\DBAL\\Types\\Types', 'STRING'),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'TEXT', 'Doctrine\\DBAL\\Types\\Types', 'TEXT'),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'BINARY', 'Doctrine\\DBAL\\Types\\Types', 'BINARY'),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'BLOB', 'Doctrine\\DBAL\\Types\\Types', 'BLOB'),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'FLOAT', 'Doctrine\\DBAL\\Types\\Types', 'FLOAT'),
+        new RenameClassAndConstFetch('Doctrine\\DBAL\\Types\\Type', 'GUID', 'Doctrine\\DBAL\\Types\\Types', 'GUID'),
+        new RenameClassAndConstFetch(
+            'Doctrine\\DBAL\\Types\\Type',
+            'DATEINTERVAL',
+            'Doctrine\\DBAL\\Types\\Types',
+            'DATEINTERVAL'
+        ),
     ]);
 };
