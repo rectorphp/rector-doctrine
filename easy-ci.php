42f35cfba27a72808838226da03bfef7c5c88d8c
Tomas Votruba <tomas.vot@gmail.com>
2023-07-13 16:17:05 +0200

    Remove ChangeSetParametersArrayToArrayCollectionRector as actually optional change

--- easy-ci.php
+++ easy-ci.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\Doctrine\Rector\Class_\ClassAnnotationToNamedArgumentConstructorRector;
-use Rector\Doctrine\Rector\MethodCall\ChangeSetParametersArrayToArrayCollectionRector;
-use Rector\Doctrine\Rector\MethodCall\EntityAliasToClassConstantReferenceRector;
-use Rector\Doctrine\CodeQuality\Rector\Property\DoctrineTargetEntityStringToClassConstantRector;
 use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
