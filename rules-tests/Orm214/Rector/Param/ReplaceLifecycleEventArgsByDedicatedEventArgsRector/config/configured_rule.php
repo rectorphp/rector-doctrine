7afed368232576de6a742339437c1e19d3dac4ff
Tomas Votruba <tomas.vot@gmail.com>
2023-07-13 22:09:52 +0200

    Move ReplaceLifecycleEventArgsByDedicatedEventArgsRector to Orm214 package (#206)

--- rules-tests/Orm214/Rector/Param/ReplaceLifecycleEventArgsByDedicatedEventArgsRector/config/configured_rule.php
+++ rules-tests/Orm214/Rector/Param/ReplaceLifecycleEventArgsByDedicatedEventArgsRector/config/configured_rule.php
@@ -3,8 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-
-use Rector\Doctrine\Rector\Param\ReplaceLifecycleEventArgsByDedicatedEventArgsRector;
+use Rector\Doctrine\Orm214\Rector\Param\ReplaceLifecycleEventArgsByDedicatedEventArgsRector;
 use Rector\Doctrine\Tests\ConfigList;
 
 return static function (RectorConfig $rectorConfig): void {
