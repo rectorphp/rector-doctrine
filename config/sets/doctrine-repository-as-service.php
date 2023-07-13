f34e482d63dc7dccc345f2e412d5d551b65b08a7
Tomas Votruba <tomas.vot@gmail.com>
2023-07-13 16:19:53 +0200

    getrector.org → getrector.com domain update (#203)

--- config/sets/doctrine-repository-as-service.php
+++ config/sets/doctrine-repository-as-service.php
@@ -19,7 +19,7 @@ use Rector\Transform\ValueObject\ReplaceParentCallByPropertyCall;
 /**
  * @see https://tomasvotruba.com/blog/2017/10/16/how-to-use-repository-with-doctrine-as-service-in-symfony/
  * @see https://tomasvotruba.com/blog/2018/04/02/rectify-turn-repositories-to-services-in-symfony/
- * @see https://getrector.org/blog/2021/02/08/how-to-instantly-decouple-symfony-doctrine-repository-inheritance-to-clean-composition
+ * @see https://getrector.com/blog/2021/02/08/how-to-instantly-decouple-symfony-doctrine-repository-inheritance-to-clean-composition
  */
 return static function (RectorConfig $rectorConfig): void {
     # order matters, this needs to be first to correctly detect parent repository
