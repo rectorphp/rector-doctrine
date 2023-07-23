281257f6dc923073cb9a7952da519fbec5943845
Tomas Votruba <tomas.vot@gmail.com>
2023-07-23 22:25:43 +0200

    Add self reference test case for DoctrineTargetEntityStringToClassConstantRector (#211)

--- rules-tests/CodeQuality/Rector/Property/DoctrineTargetEntityStringToClassConstantRector/config/configured_rule.php
+++ rules-tests/CodeQuality/Rector/Property/DoctrineTargetEntityStringToClassConstantRector/config/configured_rule.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-
 use Rector\Doctrine\CodeQuality\Rector\Property\DoctrineTargetEntityStringToClassConstantRector;
 use Rector\Doctrine\Tests\ConfigList;
 
