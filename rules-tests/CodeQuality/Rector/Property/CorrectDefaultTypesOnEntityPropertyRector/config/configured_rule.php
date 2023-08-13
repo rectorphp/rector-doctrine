49a0efa1adc5b5b4bef3daa2c1da7318f1f16d6e
Tomas Votruba <tomas.vot@gmail.com>
2023-08-13 13:21:25 +0200

    remove configs as unused

--- rules-tests/CodeQuality/Rector/Property/CorrectDefaultTypesOnEntityPropertyRector/config/configured_rule.php
+++ rules-tests/CodeQuality/Rector/Property/CorrectDefaultTypesOnEntityPropertyRector/config/configured_rule.php
@@ -6,10 +6,7 @@ use Rector\Config\RectorConfig;
 
 use Rector\Doctrine\CodeQuality\Rector\Property\CorrectDefaultTypesOnEntityPropertyRector;
 
-use Rector\Doctrine\Tests\ConfigList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(ConfigList::MAIN);
-
     $rectorConfig->rule(CorrectDefaultTypesOnEntityPropertyRector::class);
 };
