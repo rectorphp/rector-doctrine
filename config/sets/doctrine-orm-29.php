0644af7786f14e14024a5974b2994ebba8e8675b
Tomas Votruba <tomas.vot@gmail.com>
2023-09-11 10:24:43 +0200

    Make use of directly updated annotation to attributes, extra space is removed by coding standards

--- config/sets/doctrine-orm-29.php
+++ config/sets/doctrine-orm-29.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/doctrine-annotations-to-attributes.php');
+    $rectorConfig->sets([DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES]);
 };
