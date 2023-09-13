b599d7a8d207391ce74b35632a3e4a51043bc182
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-09-13 21:52:54 +0700

    Add rules-tests to ecs config (#229)

--- rules-tests/CodeQuality/Rector/ClassMethod/MakeEntitySetterNullabilityInSyncWithPropertyRector/config/configured_rule.php
+++ rules-tests/CodeQuality/Rector/ClassMethod/MakeEntitySetterNullabilityInSyncWithPropertyRector/config/configured_rule.php
@@ -6,7 +6,6 @@ use Rector\Config\RectorConfig;
 
 use Rector\Doctrine\CodeQuality\Rector\ClassMethod\MakeEntitySetterNullabilityInSyncWithPropertyRector;
 
-
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(MakeEntitySetterNullabilityInSyncWithPropertyRector::class);
 };
--- rules-tests/CodeQuality/Rector/Class_/InitializeDefaultEntityCollectionRector/config/configured_rule.php
+++ rules-tests/CodeQuality/Rector/Class_/InitializeDefaultEntityCollectionRector/config/configured_rule.php
@@ -6,7 +6,6 @@ use Rector\Config\RectorConfig;
 
 use Rector\Doctrine\CodeQuality\Rector\Class_\InitializeDefaultEntityCollectionRector;
 
-
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(InitializeDefaultEntityCollectionRector::class);
 };
