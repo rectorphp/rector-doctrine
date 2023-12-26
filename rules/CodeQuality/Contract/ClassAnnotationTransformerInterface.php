bd460cbcdf7ab351e8008f7925be639d275a295e
Tomas Votruba <tomas.vot@gmail.com>
2023-12-26 23:05:29 +0000

    Add discriminator mapping support

--- rules/CodeQuality/Contract/ClassAnnotationTransformerInterface.php
+++ rules/CodeQuality/Contract/ClassAnnotationTransformerInterface.php
@@ -7,7 +7,7 @@ namespace Rector\Doctrine\CodeQuality\Contract;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\Doctrine\CodeQuality\ValueObject\EntityMapping;
 
-interface ClassAnnotationTransformerInterface extends AnnotationTransformerInterface
+interface ClassAnnotationTransformerInterface
 {
     public function transform(EntityMapping $entityMapping, PhpDocInfo $propertyPhpDocInfo): void;
 }
