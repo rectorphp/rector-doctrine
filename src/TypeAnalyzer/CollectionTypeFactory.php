78d0c317bd605c8a31468dd9fbb16ada787a7a3f
GitHub Action <actions@github.com>
2023-12-27 00:06:20 +0000

    [rector] Rector fixes

--- src/TypeAnalyzer/CollectionTypeFactory.php
+++ src/TypeAnalyzer/CollectionTypeFactory.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Rector\Doctrine\TypeAnalyzer;
 
+use PHPStan\Type\Type;
 use PHPStan\Type\Generic\GenericObjectType;
 use PHPStan\Type\IntegerType;
 use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 
 final class CollectionTypeFactory
 {
-    public function createType(FullyQualifiedObjectType $fullyQualifiedObjectType): \PHPStan\Type\Type
+    public function createType(FullyQualifiedObjectType $fullyQualifiedObjectType): Type
     {
         $genericTypes = [new IntegerType(), $fullyQualifiedObjectType];
         return new GenericObjectType('Doctrine\Common\Collections\Collection', $genericTypes);
