477fd21da91087c4fa66a0d0992ef5317f706064
Tomas Votruba <tomas.vot@gmail.com>
2024-02-09 11:07:48 +0100

    Bump ECS to Zen Config (#258)

--- ecs.php
+++ ecs.php
@@ -2,26 +2,16 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ReturnNotation\ReturnAssignmentFixer;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (\Symplify\EasyCodingStandard\Config\ECSConfig $ecsConfig): void {
-    $ecsConfig->sets([SetList::PSR_12, SetList::SYMPLIFY, SetList::COMMON, SetList::CLEAN_CODE]);
-
-    $ecsConfig->paths([
+return ECSConfig::configure()
+    ->withPreparedSets(psr12: true, common: true, cleanCode: true, symplify: true)
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/rules',
         __DIR__ . '/rules-tests',
         __DIR__ . '/tests',
         __DIR__ . '/config',
-        __DIR__ . '/ecs.php',
-    ]);
-
-    $ecsConfig->skip([
-        '*/Source/*', '*/Fixture/*',
-        // breaks annotated code - removed on symplify dev-main
-        ReturnAssignmentFixer::class,
-    ]);
-
-    $ecsConfig->lineEnding("\n");
-};
+    ])
+    ->withRootFiles()
+    ->withSkip(['*/Source/*', '*/Fixture/*']);
