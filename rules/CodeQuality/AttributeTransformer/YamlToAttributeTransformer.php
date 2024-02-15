28a824c80691d49eaeddf5d5ffb5fd2dba49ed3e
Tomas Votruba <tomas.vot@gmail.com>
2024-02-15 01:49:00 +0100

    skip already added attributes

--- rules/CodeQuality/AttributeTransformer/YamlToAttributeTransformer.php
+++ rules/CodeQuality/AttributeTransformer/YamlToAttributeTransformer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\Doctrine\CodeQuality\AttributeTransformer;
 
 use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Property;
 use Rector\Doctrine\CodeQuality\Contract\ClassAttributeTransformerInterface;
 use Rector\Doctrine\CodeQuality\Contract\PropertyAttributeTransformerInterface;
 use Rector\Doctrine\CodeQuality\ValueObject\EntityMapping;
@@ -24,13 +25,16 @@ final readonly class YamlToAttributeTransformer
     public function transform(Class_ $class, EntityMapping $entityMapping): void
     {
         $this->transformClass($class, $entityMapping);
-
         $this->transformProperties($class, $entityMapping);
     }
 
     private function transformClass(Class_ $class, EntityMapping $entityMapping): void
     {
         foreach ($this->classAttributeTransformers as $classAttributeTransformer) {
+            if ($this->hasAttribute($class, $classAttributeTransformer->getClassName())) {
+                continue;
+            }
+
             $classAttributeTransformer->transform($entityMapping, $class);
         }
     }
@@ -39,8 +43,24 @@ final readonly class YamlToAttributeTransformer
     {
         foreach ($class->getProperties() as $property) {
             foreach ($this->propertyAttributeTransformers as $propertyAttributeTransformer) {
+                if ($this->hasAttribute($property, $propertyAttributeTransformer->getClassName())) {
+                    continue;
+                }
+
                 $propertyAttributeTransformer->transform($entityMapping, $property);
             }
         }
+    }
+
+    private function hasAttribute(Class_|Property $stmt, string $attributeClassName): bool
+    {
+        foreach ($stmt->attrGroups as $attrGroup) {
+            foreach ($attrGroup->attrs as $attr) {
+                if ($attr->name->toString() === $attributeClassName) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
