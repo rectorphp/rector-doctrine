b8e34c7a9f690af4d666ac8c93b4ec89b1f8143b
Julien Fastré <julienfastre@users.noreply.github.com>
2024-03-07 14:55:53 +0100

    Add Doctrine Collection 2.2 set with a configure rule to rename constants associated with 'Doctrine\Common\Collections\Criteria' (#297)
    This commit introduces new doctrine-collection-2.2 configuration file and unit tests. It adds rules for renaming class and constant fetch from 'Doctrine\Common\Collections\Criteria' to 'Doctrine\Common\Collections\Order' in the rector configuration. The commit also includes the declaration of this set in the DoctrineSetList.

---
+++ tests/Set/DoctrineCOLLECTION22Set/DoctrineCOLLECTION22SetTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rector\Doctrine\Tests\Set\DoctrineCOLLECTION22Set;
+
+use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+class DoctrineCOLLECTION22SetTest extends AbstractRectorTestCase
+{
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_set.php';
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+}
