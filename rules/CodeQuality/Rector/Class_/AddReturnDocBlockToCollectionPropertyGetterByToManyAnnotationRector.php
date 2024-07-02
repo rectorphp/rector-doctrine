78fa974aa2ced5f60030424a296b2d13c09eafdf
Jarosław Szutkowski <jsz.wwa@gmail.com>
2024-07-02 08:35:58 +0200

    Add return doc block to Collection property's getter method by ToMany annotation (#322)
    * Add return doc block to Collection property's getter method by ToMany annotation
    
    * more tests
    
    * more tests
    
    * more tests
    
    * regenerate docs
    
    * renamed fixture file
    
    * feedback

--- rules/CodeQuality/Rector/Class_/AddReturnDocBlockToCollectionPropertyGetterByToManyAnnotationRector.php
+++ rules/CodeQuality/Rector/Class_/AddReturnDocBlockToCollectionPropertyGetterByToManyAnnotationRector.php
@@ -5,37 +5,32 @@ declare(strict_types=1);
 namespace Rector\Doctrine\CodeQuality\Rector\Class_;
 
 use PhpParser\Node;
-use PhpParser\Node\Attribute;
-use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Property;
 use PHPStan\Analyser\Scope;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
 use Rector\BetterPhpDocParser\PhpDocManipulator\PhpDocTypeChanger;
-use Rector\Doctrine\CodeQuality\Enum\ToManyMappings;
-use Rector\Doctrine\NodeAnalyzer\AttributeFinder;
 use Rector\Doctrine\NodeAnalyzer\MethodUniqueReturnedPropertyResolver;
-use Rector\Doctrine\NodeAnalyzer\TargetEntityResolver;
+use Rector\Doctrine\PhpDocParser\DoctrineDocBlockResolver;
 use Rector\Doctrine\TypeAnalyzer\CollectionTypeFactory;
+use Rector\Doctrine\TypeAnalyzer\CollectionTypeResolver;
 use Rector\Rector\AbstractScopeAwareRector;
 use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
-use Rector\ValueObject\PhpVersionFeature;
 use Rector\VendorLocker\NodeVendorLocker\ClassMethodReturnTypeOverrideGuard;
-use Rector\VersionBonding\Contract\MinPhpVersionInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
- *  @see \Rector\Doctrine\Tests\CodeQuality\Rector\Class_\AddReturnDocBlockToCollectionPropertyGetterByToManyAttributeRector\AddReturnDocBlockToCollectionPropertyGetterByToManyAttributeRectorTest
+ *  @see \Rector\Doctrine\Tests\CodeQuality\Rector\Class_\AddReturnDocBlockToCollectionPropertyGetterByToManyAnnotationRector\AddReturnDocBlockToCollectionPropertyGetterByToManyAnnotationRectorTest
  */
-final class AddReturnDocBlockToCollectionPropertyGetterByToManyAttributeRector extends AbstractScopeAwareRector implements MinPhpVersionInterface
+final class AddReturnDocBlockToCollectionPropertyGetterByToManyAnnotationRector extends AbstractScopeAwareRector
 {
     public function __construct(
         private readonly ClassMethodReturnTypeOverrideGuard $classMethodReturnTypeOverrideGuard,
         private readonly PhpDocInfoFactory $phpDocInfoFactory,
+        private readonly DoctrineDocBlockResolver $doctrineDocBlockResolver,
         private readonly PhpDocTypeChanger $phpDocTypeChanger,
-        private readonly AttributeFinder $attributeFinder,
-        private readonly TargetEntityResolver $targetEntityResolver,
+        private readonly CollectionTypeResolver $collectionTypeResolver,
         private readonly CollectionTypeFactory $collectionTypeFactory,
         private readonly MethodUniqueReturnedPropertyResolver $methodUniqueReturnedPropertyResolver,
     ) {
@@ -43,13 +38,17 @@ final class AddReturnDocBlockToCollectionPropertyGetterByToManyAttributeRector e
 
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('Adds @return PHPDoc type to Collection property getter by *ToMany attribute', [
+        return new RuleDefinition('Adds @return PHPDoc type to Collection property getter by *ToMany annotation', [
             new CodeSample(
                 <<<'CODE_SAMPLE'
-#[ORM\Entity]
+/**
+ * @ORM\Entity
+ */
 final class Trainer
 {
-    #[ORM\OneToMany(targetEntity:Training::class, mappedBy:"trainer")]
+    /**
+     * @ORM\OneToMany(targetEntity=Training::class, mappedBy="trainer")
+     */
     private $trainings;
 
     public function getTrainings()
@@ -60,14 +59,18 @@ final class Trainer
 CODE_SAMPLE
                 ,
                 <<<'CODE_SAMPLE'
-#[ORM\Entity]
+/**
+ * @ORM\Entity
+ */
 final class Trainer
 {
-    #[ORM\OneToMany(targetEntity:Training::class, mappedBy:"trainer")]
+    /**
+     * @ORM\OneToMany(targetEntity=Training::class, mappedBy="trainer")
+     */
     private $trainings;
 
     /**
-     * @return \Doctrine\Common\Collections\Collection<int, \Rector\Doctrine\Tests\CodeQuality\Rector\Property\ImproveDoctrineCollectionDocTypeInEntityRector\Source\Training>
+     * @return \Doctrine\Common\Collections\Collection<int, \Rector\Doctrine\Tests\CodeQuality\Rector\Class_\AddReturnDocBlockToCollectionPropertyGetterByToManyAnnotationRector\Source>
      */
     public function getTrainings()
     {
@@ -92,7 +95,7 @@ CODE_SAMPLE
      */
     public function refactorWithScope(Node $node, Scope $scope): ?Node
     {
-        if (! $this->isDoctrineEntityClass($node)) {
+        if (! $this->doctrineDocBlockResolver->isDoctrineEntityClass($node)) {
             return null;
         }
 
@@ -109,7 +112,7 @@ CODE_SAMPLE
                 continue;
             }
 
-            $collectionObjectType = $this->getCollectionObjectTypeFromToManyAttribute($property);
+            $collectionObjectType = $this->collectionTypeResolver->resolveFromToManyProperties($property);
 
             if (! $collectionObjectType instanceof FullyQualifiedObjectType) {
                 continue;
@@ -122,41 +125,5 @@ CODE_SAMPLE
         }
 
         return $hasChanged ? $node : null;
-    }
-
-    public function provideMinPhpVersion(): int
-    {
-        return PhpVersionFeature::ATTRIBUTES;
-    }
-
-    private function getCollectionObjectTypeFromToManyAttribute(Property $property): ?FullyQualifiedObjectType
-    {
-        $targetEntityExpr = $this->attributeFinder->findAttributeByClassesArgByName(
-            $property,
-            ToManyMappings::TO_MANY_CLASSES,
-            'targetEntity'
-        );
-
-        if (! $targetEntityExpr instanceof ClassConstFetch) {
-            return null;
-        }
-
-        $targetEntityClassName = $this->targetEntityResolver->resolveFromExpr($targetEntityExpr);
-
-        if ($targetEntityClassName === null) {
-            return null;
-        }
-
-        return new FullyQualifiedObjectType($targetEntityClassName);
-    }
-
-    private function isDoctrineEntityClass(Class_ $class): bool
-    {
-        $entityAttribute = $this->attributeFinder->findAttributeByClasses(
-            $class,
-            ['Doctrine\ORM\Mapping\Entity', 'Doctrine\ORM\Mapping\Embeddable'],
-        );
-
-        return $entityAttribute instanceof Attribute;
     }
 }
