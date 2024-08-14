cec46f77f4632642b3c404fce4fb381b95ce8250
Tomas Votruba <tomas.vot@gmail.com>
2024-08-15 00:32:53 +0200

    add fixutre for var and attribute

--- rules-tests/CodeQuality/Rector/Property/ImproveDoctrineCollectionDocTypeInEntityRector/config/configured_rule.php
+++ rules-tests/CodeQuality/Rector/Property/ImproveDoctrineCollectionDocTypeInEntityRector/config/configured_rule.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\CodeQuality\Rector\Property\ImproveDoctrineCollectionDocTypeInEntityRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(ImproveDoctrineCollectionDocTypeInEntityRector::class);
-};
+return RectorConfig::configure()
+    ->withRules([ImproveDoctrineCollectionDocTypeInEntityRector::class])
+    ->withPhpVersion(\Rector\ValueObject\PhpVersionFeature::ATTRIBUTES);
