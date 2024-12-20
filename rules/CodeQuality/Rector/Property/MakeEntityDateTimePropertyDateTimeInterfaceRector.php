e29ed08e7f476f8fab1d71601050d798c7f63e61
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-20 22:30:33 +0700

    [CodeQuality] Deprecate MakeEntityDateTimePropertyDateTimeInterfaceRector (#358)
    * [CodeQuality] Deprecate MakeEntityDateTimePropertyDateTimeInterfaceRector
    
    * fix phpstan
    
    * implements DeprecatedInterface

--- rules/CodeQuality/Rector/Property/MakeEntityDateTimePropertyDateTimeInterfaceRector.php
+++ rules/CodeQuality/Rector/Property/MakeEntityDateTimePropertyDateTimeInterfaceRector.php
@@ -13,6 +13,7 @@ use PHPStan\Type\UnionType;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
 use Rector\Comments\NodeDocBlock\DocBlockUpdater;
+use Rector\Configuration\Deprecation\Contract\DeprecatedInterface;
 use Rector\Doctrine\NodeAnalyzer\DoctrineEntityDetector;
 use Rector\NodeTypeResolver\PhpDoc\NodeAnalyzer\DocBlockClassRenamer;
 use Rector\NodeTypeResolver\ValueObject\OldToNewType;
@@ -25,8 +26,10 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  * @see related to maker bundle https://symfony.com/doc/current/bundles/SymfonyMakerBundle/index.html
  *
  * @see \Rector\Doctrine\Tests\CodeQuality\Rector\Property\MakeEntityDateTimePropertyDateTimeInterfaceRector\MakeEntityDateTimePropertyDateTimeInterfaceRectorTest
+ *
+ * @deprecated see https://github.com/rectorphp/rector-doctrine/issues/356 for reasoning
  */
-final class MakeEntityDateTimePropertyDateTimeInterfaceRector extends AbstractRector
+final class MakeEntityDateTimePropertyDateTimeInterfaceRector extends AbstractRector implements DeprecatedInterface
 {
     public function __construct(
         private readonly DocBlockClassRenamer $docBlockClassRenamer,
