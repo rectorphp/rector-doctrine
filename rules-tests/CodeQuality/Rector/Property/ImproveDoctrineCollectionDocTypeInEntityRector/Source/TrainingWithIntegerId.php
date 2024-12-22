f84e8a5dab7a367b41d0d6c248d59f815b99c442
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-22 18:52:05 +0700

    add integer type fixture
    fix extension
    
    add attribute

---
+++ rules-tests/CodeQuality/Rector/Property/ImproveDoctrineCollectionDocTypeInEntityRector/Source/TrainingWithIntegerId.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Doctrine\Tests\CodeQuality\Rector\Property\ImproveDoctrineCollectionDocTypeInEntityRector\Source;
+
+use Doctrine\ORM\Mapping as ORM;
+
+final class TrainingWithIntegerId
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     */
+    private int $id;
+}
