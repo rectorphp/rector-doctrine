d9be91e8fac7f903b15c59f061df243711693fce
Johan Vlaar <JohJohan@users.noreply.github.com>
2025-03-06 13:09:31 +0100

    [ORM-300] add missing set (#371)

--- config/sets/doctrine-orm-300.php
+++ config/sets/doctrine-orm-300.php
@@ -3,9 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Doctrine\Orm30\Rector\MethodCall\SetParametersArrayToCollectionRector;
 use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rules([
+        SetParametersArrayToCollectionRector::class,
+    ]);
+
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
         'Doctrine\ORM\ORMException' => 'Doctrine\ORM\Exception\ORMException',
     ]);
