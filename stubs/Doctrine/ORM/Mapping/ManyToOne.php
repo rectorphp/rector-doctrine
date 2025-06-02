bcb79b9fbb9863adf062d0d251e9d0730eb24520
Tomas Votruba <tomas.vot@gmail.com>
2025-06-02 15:46:38 +0200

    attribute

--- stubs/Doctrine/ORM/Mapping/ManyToOne.php
+++ stubs/Doctrine/ORM/Mapping/ManyToOne.php
@@ -11,6 +11,7 @@ if (class_exists('Doctrine\ORM\Mapping\ManyToOne')) {
 /**
  * @see https://github.com/doctrine/orm/blob/2.12.x/lib/Doctrine/ORM/Mapping/ManyToOne.php
  */
+#[\Attribute(\Attribute::TARGET_PROPERTY)]
 final class ManyToOne
 {
     public function __construct(
