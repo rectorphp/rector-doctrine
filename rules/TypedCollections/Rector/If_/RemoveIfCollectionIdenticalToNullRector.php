64d9afccff7af0e2b7ec6f12059b5506103f0106
Tomas Votruba <tomas.vot@gmail.com>
2025-06-12 19:30:08 +0200

    add boolean and not null case

--- rules/TypedCollections/Rector/If_/RemoveIfCollectionIdenticalToNullRector.php
+++ rules/TypedCollections/Rector/If_/RemoveIfCollectionIdenticalToNullRector.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Rector\Doctrine\TypedCollections\Rector\If_;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\BinaryOp\BooleanAnd;
 use PhpParser\Node\Expr\BinaryOp\BooleanOr;
 use PhpParser\Node\Expr\BinaryOp\Identical;
-use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Expr\BinaryOp\NotIdentical;
 use PhpParser\Node\Stmt\If_;
 use Rector\Doctrine\TypedCollections\TypeAnalyzer\CollectionTypeDetector;
+use Rector\PhpParser\Node\Value\ValueResolver;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -21,6 +24,7 @@ final class RemoveIfCollectionIdenticalToNullRector extends AbstractRector
 {
     public function __construct(
         private readonly CollectionTypeDetector $collectionTypeDetector,
+        private readonly ValueResolver $valueResolver,
     ) {
 
     }
@@ -35,31 +39,25 @@ final class RemoveIfCollectionIdenticalToNullRector extends AbstractRector
      */
     public function refactor(Node $node): ?If_
     {
-        if (! $node->cond instanceof BooleanOr) {
-            return null;
+        if ($node->cond instanceof BooleanOr) {
+            $changedCond = $this->refactorBooleanOr($node->cond);
+            if ($changedCond instanceof Expr) {
+                $node->cond = $changedCond;
+
+                return $node;
+            }
         }
 
-        $leftCondition = $node->cond->left;
-        if (! $leftCondition instanceof Identical) {
-            return null;
+        if ($node->cond instanceof BooleanAnd) {
+            $changedCond = $this->refactorBooleanAnd($node->cond);
+            if ($changedCond instanceof Expr) {
+                $node->cond = $changedCond;
+
+                return $node;
+            }
         }
 
-        if (! $leftCondition->right instanceof ConstFetch || ! $this->isName(
-            $leftCondition->right->name,
-            'null'
-        )) {
-            return null;
-        }
-
-        if (! $this->collectionTypeDetector->isCollectionType($leftCondition->left)) {
-            return null;
-        }
-
-        $rightCondition = $node->cond->right;
-
-        $node->cond = $rightCondition;
-
-        return $node;
+        return null;
     }
 
     public function getRuleDefinition(): RuleDefinition
@@ -102,5 +100,41 @@ final class SomeClass
 }
 CODE_SAMPLE
             )]);
+    }
+
+    private function refactorBooleanOr(BooleanOr $booleanOr): ?Expr
+    {
+        $leftCondition = $booleanOr->left;
+        if (! $leftCondition instanceof Identical) {
+            return null;
+        }
+
+        if (! $this->valueResolver->isNull(($leftCondition->right))) {
+            return null;
+        }
+
+        if (! $this->collectionTypeDetector->isCollectionType($leftCondition->left)) {
+            return null;
+        }
+
+        return $booleanOr->right;
+    }
+
+    private function refactorBooleanAnd(BooleanAnd $booleanAnd): ?Expr
+    {
+        $leftCondition = $booleanAnd->left;
+        if (! $leftCondition instanceof NotIdentical) {
+            return null;
+        }
+
+        if (! $this->valueResolver->isNull($leftCondition->right)) {
+            return null;
+        }
+
+        if (! $this->collectionTypeDetector->isCollectionType($leftCondition->left)) {
+            return null;
+        }
+
+        return $booleanAnd->right;
     }
 }
