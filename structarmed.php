503035d40ea785beae1e5a8fc5141fcf75bff307
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-27 01:21:20 +0700

    [QA] Add StructArmed to QA

---
+++ structarmed.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Boundwize\StructArmed\Architecture;
+use Boundwize\StructArmed\Preset\Preset;
+
+return Architecture::define()
+    ->withPresets(Preset::PSR1(), Preset::PSR4());
