9fe4b11d54a658ff9150c4a6eb78fcb38f722941
Matthieu Lempereur <matt.lempereur@gmail.com>
2026-07-14 17:27:08 +0200

    [DataFixtures 17] add new set for doctrine data-fixtures 17 (#487)

---
+++ config/sets/data-fixtures-17.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPStan\Type\IntegerType;
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
+
+return static function (RectorConfig $rectorConfig): void {
+
+    $rectorConfig->ruleWithConfiguration(AddReturnTypeDeclarationRector::class, [
+        new AddReturnTypeDeclaration('Doctrine\Common\DataFixtures\OrderedFixtureInterface', 'getOrder', new IntegerType()),
+    ]);
+};
