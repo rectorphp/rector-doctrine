5d01d2c24c91803a26486141242c628354569782
Tomas Votruba <tomas.vot@gmail.com>
2026-09-08 17:41:16 +0200

    [CodeQuality] Skip TypedPropertyFromColumnTypeRector on untyped parent property override (#521)

---
+++ rules-tests/CodeQuality/Rector/Property/TypedPropertyFromColumnTypeRector/Source/AbstractParentScopeEntity.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Doctrine\Tests\CodeQuality\Rector\Property\TypedPropertyFromColumnTypeRector\Source;
+
+abstract class AbstractParentScopeEntity extends AbstractGrandParentScopeEntity
+{
+}
